b9d85ec4611a0b403a3027277d2ad545a4adf428
Michael Lesniak <mail@mlesniak.com>
2018-02-19 06:05:02 +0100

    Add database code

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import ("fmt")
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	// This is the usual way to include an SQL driver in golang. Actually we are not using
+	// any imports from the package explictly.
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
-    fmt.Println("Hello, sqllite")
+	database, _ := sql.Open("sqlite3", "./demo.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement.Exec()
+
+	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	statement.Exec("Michael", "Lesniak")
+
+	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	var id int
+	var firstname string
+	var lastname string
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
 }
